35e68d15e4ca8fe68eedc95ae4bac5bb54c5aea6
Abareyellow <yellowranger2000@gmail.com>
2018-07-27 12:33:47 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,37 @@
 # Code your instances here
+class GardenGnome
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def age=(age)
+    @age = age 
+  end
+  
+  def gluten_allergy
+    @gluten_allergy
+  end
+  
+  def initialize(personality="evil", hat_color="red")
+    @personality = personality
+    @hat_color = hat_color
+  end
+  
+  def gnaw
+    return "Gnawing on a tree!!"
+  end
+  
+  def shout
+    return "GNARLY"
+  end
+  
+  def introduce_self
+    return "Hello humans, my name is #{name}, I am #{age} year old, and you'll rue the day you crossed me!"
+  end
+  
+gnome1 = GardenGnome
+gnome2 = GardenGnome.new("Walter the Worst")
+gnome3 = GardenGnome.new("James the Jerk", "3421")
+gnome4 = GardenGnome.new("Alfred the Abhorrent", "579", )
+end
